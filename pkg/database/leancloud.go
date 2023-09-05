8380cadf339eb829ab7d6396c0c95e8d8100e6fd
Boruiqiao <39862938+qiaoborui@users.noreply.github.com>
2023-09-05 14:30:22 +0000

    fix filter

--- pkg/database/leancloud.go
+++ pkg/database/leancloud.go
@@ -1,15 +1,12 @@
 package database
 
 import (
-	"encoding/json"
 	"fmt"
-	"github.com/leancloud/go-sdk/leancloud"
-	"github.com/pkg/errors"
-	"io"
-	"net/http"
-	"net/url"
 	"sort"
 	"wobuzaixiaoyuan/pkg/common"
+
+	"github.com/leancloud/go-sdk/leancloud"
+	"github.com/pkg/errors"
 )
 
 var Client *leancloud.Client
@@ -18,40 +15,9 @@ type Response struct {
 	Results []User `json:"results"`
 }
 
-func FetchData(condition string) (Response, error) {
-	client := &http.Client{}
-	Url, _ := url.Parse(common.APPURL)
-	params := url.Values{}
-	params.Set("where", condition)
-	Url.RawQuery = params.Encode()
-	urlPath := Url.String()
-	req, _ := http.NewRequest("GET", urlPath, nil)
-	req.Header.Add("X-LC-Id", common.APPID)
-	req.Header.Add("X-LC-Key", common.APPKEY)
-	req.Header.Add("Content-Type", "application/json")
-	resp, err := client.Do(req)
-	if err != nil {
-		return Response{}, err
-	}
-	defer resp.Body.Close()
-	body, _ := io.ReadAll(resp.Body)
-	var response Response
-	err = json.Unmarshal(body, &response)
-	if err != nil {
-		return Response{}, err
-	}
-	sort.Slice(response.Results, func(i, j int) bool {
-		if response.Results[i].IsEnable > response.Results[j].IsEnable {
-			return true
-		}
-		return false
-	})
-	return response, nil
-}
-
 func GetUsers() ([]User, error) {
 	var users []User
-	err := Client.Class("InSchool").NewQuery().Find(&users)
+	err := Client.Class("InSchool").NewQuery().EqualTo("status", 1).Find(&users)
 	if err != nil {
 		fmt.Println(err)
 		return nil, errors.Wrap(err, "获取用户数据失败")
